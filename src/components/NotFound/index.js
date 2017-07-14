da6cef32177c4479fece91fa3c33d5e6fc41da6e
Marc Altmann <marcaltmann@posteo.de>
2017-07-14 22:58:07 +0200

    Initial commit

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,26 @@
+// src/components/NotFound/index.js
+import React, { PropTypes, Component } from 'react';
+import classnames from 'classnames';
+import { Link } from 'react-router';
+
+import './style.css';
+
+export default class NotFound extends Component {
+  // static propTypes = {}
+  // static defaultProps = {}
+  // state = {}
+
+  render() {
+    const { className, ...props } = this.props;
+    return (
+      <div className={classnames('NotFound', className)} {...props}>
+        <h1>
+          404 <small>Not Found :(</small>
+        </h1>
+        <p>
+          <Link to="/about">About</Link>
+        </p>
+      </div>
+    );
+  }
+}
